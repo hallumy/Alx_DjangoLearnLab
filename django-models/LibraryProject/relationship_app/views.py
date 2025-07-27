e1e8facba3ee331e026abb5c7f8b7022d22245b9
Ahlam Abdulkhalek <alhamny@gmail.com>
2025-07-27 18:28:33 +0300

    upfate views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from relationship_app.models import book
 # Create your views here.
+
+def list_books(request):
+    """Function-based view that returns all books and their authors as plain text"""
+    books = Book.objects.get('author').all()
+    return render(request, 'relationship_app/book_list.html', {'books': books})
+    
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = "relationship_app/library_detail.html"
+    context_object_name = "library"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["books"] = self.object.books.select_related('author').all()
+        return context
+
