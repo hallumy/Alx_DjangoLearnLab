2c15cfe32a4da0e42d14a445b389ffe6d1ad4d3e
Ahlam Abdulkhalek <alhamny@gmail.com>
2025-07-30 22:42:56 +0300

    Create login page

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -5,7 +5,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', views.list_books, name='list_books'),                 # Function-based view
     path('', LibraryDetailView.as_view(), name='library_detail'),  # Class-based view
-    path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
-    path('register/', views.register_view, name='register'),
+    path('login/', LoginView.as_view(), template_name='login'),
+    path('logout/', LogoutView.as_view(), template_name='logout'),
+    path('register/', views.register_view, template_name='register'),
 ]
