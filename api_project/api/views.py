8a13f9d03797d81a948cc1b37cb7bd30ff4d2c1e
Ahlam Abdulkhalek <alhamny@gmail.com>
2025-08-03 04:37:02 +0300

    Add Serializers

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,8 +3,8 @@ from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 
-class BookList(generics.ListCreateAPIView):
+class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-# Create your views here.
+
