293a09874fed6dbac4159091dee88d66858e52df
Ahlam Abdulkhalek <alhamny@gmail.com>
2025-08-19 10:43:11 +0300

    implement user authentication system

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -7,3 +7,10 @@ class Post(models.Model):
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True, null=True)
+    profile_picture = models.ImageField(upload_to="profile_pics/", blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s profile"
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,36 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from .forms import CustomUserCreationForm
+
+def register(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("profile")
+    else:
+        form = CustomUserCreationForm()
+
+    return render(request, 'auth/register.html', {"form":form})
+
+@login_required
+def profile(request):
+    if request.method == "POST":
+        request.user.email = request.POST,get("email")
+        request.user.save()
+        return redirect("profile")
+    
+    return render(request, "auth/profile", {"user": request.user})
+    
+def home(request):
+    return render(request, "blog/home.html")
+
 from django.shortcuts import render
 
-# Create your views here.
+def posts(request):
+    return render(request, "blog/posts.html")
+
+        
+
