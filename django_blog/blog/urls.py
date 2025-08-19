12338ce0340ae05934e0995650ce8682c580536b
Ahlam Abdulkhalek <alhamny@gmail.com>
2025-08-19 23:02:58 +0300

    Add blog post management features

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
+from .views import CreatePost,DeletePost,ListPost,PostDetail,UpdatePost
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('register/', views.register, name='register'),
     path('profile/', views.profile, name='profile'),
     path('home/', views.home, name='home'),
-    path('posts/', views.posts, name='posts')
+    path('posts/', ListPost.as_view(), name='post_list'),                
+    path('posts/create/', CreatePost.as_view(), name='create_post'),
+    path('posts/<int:pk>/', PostDetail.as_view(), name="post_detail"),   
+    path('posts/<int:pk>/update/', UpdatePost.as_view(), name="post_update"), 
+    path('posts/<int:pk>/delete/', DeletePost.as_view(), name="post_delete"),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
 from .forms import CustomUserCreationForm
+from .models import Post
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 def register(request):
     if request.method == "POST":
@@ -13,24 +17,51 @@ def register(request):
     else:
         form = CustomUserCreationForm()
 
-    return render(request, 'auth/register.html', {"form":form})
+    return render(request, 'blog/register.html', {"form":form})
 
 @login_required
 def profile(request):
     if request.method == "POST":
-        request.user.email = request.POST,get("email")
+        request.user.email = request.POST.get("email")
         request.user.save()
         return redirect("profile")
     
-    return render(request, "auth/profile", {"user": request.user})
+    return render(request, "blog/profile.html", {"user": request.user})
     
 def home(request):
     return render(request, "blog/home.html")
 
-from django.shortcuts import render
-
 def posts(request):
-    return render(request, "blog/posts.html")
+    posts = Post.objects.all()
+    return render(request, "blog/posts.html", {"posts":posts})
 
-        
+class CreatePost(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content', 'published_date', 'author']
+    template_name = 'blog/Post_form.html'
+    
+    def form_valid(self, form):
+        # Automatically set the author as the logged-in user
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+class ListPost(ListView):
+    model = Post
+    template_name = 'blog/posts.html'
+    context_object_name = 'posts'
+    
+class PostDetail(DetailView):
+    model = Post
+
+class UpdatePost(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class DeletePost(DeleteView):
+    model = Post 
+    template_name = "blog/post_delete.html"
+    success_url = reverse_lazy("post_list")
+
 
